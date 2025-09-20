5870e1b3862e9bf6447866cf18645d7ffc8a69cc
Jeremy <jeremy.yosep@gmail.com>
2025-09-20 21:52:50 +0700

    Update Repository

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,31 +1,24 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.project.tamanbacaan"
-    compileSdk = 36
+    namespace = "com.caffeinatedr4t.tamanbacaan"
+    compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.project.tamanbacaan"
-        minSdk = 33
-        targetSdk = 36
+        applicationId = "com.caffeinatedr4t.tamanbacaan"
+        minSdk = 21
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+    buildFeatures {
+        viewBinding = true
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -35,14 +28,23 @@ android {
     }
 }
 
-dependencies {
+repositories {
+    google()
+    mavenCentral()
+}
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+dependencies {
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
---
+++ app/src/main/java/com/project/tamanbacaan/activities/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.project.tamanbacaan.activities
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.project.tamanbacaan.R
+import com.project.tamanbacaan.fragments.HomeFragment
+import com.project.tamanbacaan.fragments.SearchFragment
+import com.project.tamanbacaan.fragments.BookmarkFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Load home fragment by default
+        loadFragment(HomeFragment())
+
+        // Set up bottom navigation
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        findViewById<View>(R.id.btnHome).setOnClickListener {
+            loadFragment(HomeFragment())
+        }
+        findViewById<View>(R.id.btnSearch).setOnClickListener {
+            loadFragment(SearchFragment())
+        }
+        findViewById<View>(R.id.btnBookmark).setOnClickListener {
+            loadFragment(BookmarkFragment())
+        }
+        findViewById<View>(R.id.btnExplore).setOnClickListener {
+            loadFragment(SearchFragment())
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment, fragment)
+        transaction.commit()
+    }
+}
