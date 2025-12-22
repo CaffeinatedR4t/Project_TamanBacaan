66594fe1b35d6176c7bf0abc40813aa319a9379c
YuutoRaimi <playzyehuda@gmail.com>
2025-12-22 22:11:15 +0700

    fix dikit

--- app/src/main/java/com/project/tamanbacaan/utils/Constants.kt
+++ app/src/main/java/com/project/tamanbacaan/utils/Constants.kt
@@ -4,14 +4,12 @@ object Constants {
     const val APP_NAME = "Taman Bacaan"
     const val DATABASE_VERSION = 1
 
-    // Fragment Tags
     const val FRAGMENT_HOME = "fragment_home"
     const val FRAGMENT_SEARCH = "fragment_search"
     const val FRAGMENT_BOOKMARK = "fragment_bookmark"
     const val KEY_USER = "key_user"
     const val EXTRA_BOOK_ID = "book_id"
 
-    // ========== ADD THESE NEW LINES ==========
     // API Configuration - For Android Emulator (API 33 Tiramisu)
     const val BASE_URL = "http://10.0.2.2:3000/api/"
 
